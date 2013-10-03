6a1e4cf7ad9d48cd15e747c277261533139569f0
nhatnguyen26 <nathan.nguyen2604@gmail.com>
2013-10-03 03:26:53 -0700

    partI finished

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,7 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.sorting(params)
   end
 
   def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,11 @@
 class Movie < ActiveRecord::Base
+	scope :order_by, lambda {|col| order("#{col} ASC")}
+	
+	def self.sorting(params)
+		if !params[:sort_by].blank?
+			movies = Movie.order_by(params[:sort_by])
+		else
+			movies = Movie.all
+		end
+	end
 end
